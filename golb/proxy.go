3a289785eaf2500d374337428baefe9e0be036c5
Sergey <xxsmotur@gmail.com>
2018-09-20 18:37:08 +0500

    Draft for implementing choicing of servers on reverse proxy

--- golb/proxy.go
+++ golb/proxy.go
@@ -3,6 +3,7 @@ package golb
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/saromanov/golb/server"
 )
@@ -14,12 +15,18 @@ type HTTPProxy struct {
 
 // Do provides executing of the proxy
 func (p *HTTPProxy) Do(w http.ResponseWriter, r *http.Request) error {
+	u, err := url.Parse("http://" + p.serv.Host + r.RequestURI)
+	if err != nil {
+		return err
+	}
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, v []*http.Request) error {
 			return http.ErrUseLastResponse
 		},
 	}
-	r.Host = p.serv.Host
+	r.Header.Set("X-Forwarded-Host", "127.0.0.1")
+	r.Host = "127.0.0.1"
+	r.URL = u
 	r.RequestURI = ""
 	resp, err := client.Do(r)
 	if err != nil {
