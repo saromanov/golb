afab963d67881b19241d8ca163d5938d007091c8
Sergey <xxsmotur@gmail.com>
2018-12-18 23:57:39 +0500

    Add ID to server

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package server
 
 // Server defines struct for the server definition
 type Server struct {
+	ID 				  string
 	Host              string
 	Port              uint32
 	ActiveConnections uint32
