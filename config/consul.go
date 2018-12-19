7c968989de9b356f2cda882d93c600783769e87a
Sergey <xxsmotur@gmail.com>
2018-12-19 22:40:30 +0500

    Complete method for read config from Consul

--- config/consul.go
+++ config/consul.go
@@ -1,29 +1,28 @@
 package config
 
-import "github.com/hashicorp/consul/api"
+import (
+	"fmt"
+	"encoding/json"
+	"github.com/hashicorp/consul/api"
+)
 
 // ReadConsulConfig provides reading of config from Comsul
 func ReadConsulConfig(key string) (*Config, error) {
-	// Get a new client
 	client, err := api.NewClient(api.DefaultConfig())
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("Consul: unable to create client: %v", err)
 	}
-
-	// Get a handle to the KV API
 	kv := client.KV()
 
-	// PUT a new KV pair
-	p := &api.KVPair{Key: "REDIS_MAXCLIENTS", Value: []byte("1000")}
-	_, err = kv.Put(p, nil)
+	pair, _, err := kv.Get(key, nil)
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("Consul: unable to get value by the key: %v", err)
 	}
 
-	// Lookup the pair
-	pair, _, err := kv.Get("REDIS_MAXCLIENTS", nil)
+	var c*Config
+	err = json.Unmarshal(pair.Value, &c)
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("Consul: unable to unmarshal response: %v", err)
 	}
-	fmt.Printf("KV: %v %s\n", pair.Key, pair.Value)
+	return c, nil
 }
