941a86f5cc0968a6a9741379ad9768d97905677b
Sergey <xxsmotur@gmail.com>
2018-12-29 23:27:10 +0500

    Discovery.json: implementation of getservers method

--- discovery/json/json.go
+++ discovery/json/json.go
@@ -2,26 +2,39 @@ package json
 
 import (
 	"errors"
-	"github.com/saromanov/golb/server"
+
 	"github.com/saromanov/golb/config"
+	"github.com/saromanov/golb/server"
 )
 
 var errEmptyConfig = errors.New("config is not defined")
 
 // Discovery provides importing of servers based on json
 type Discovery struct {
-	servers []server.Server
+	servers []*server.Server
 }
 
 // New provides getting of server definition from config file
-func New(c* config.Config)(*Discovery, error) {
+func New(c *config.Config) (*Discovery, error) {
 	if c == nil {
 		return nil, errEmptyConfig
 	}
 
+	srvs := getServers(c)
+	return &Discovery{
+		servers: srvs,
+	}, nil
+
 }
 
 // getServers returns servers from config
-func getServers(c *config.Config)[]server.Server {
-
+func getServers(c *config.Config) []*server.Server {
+	result := make([]*server.Server, len(c.Servers))
+	for i, s := range c.Servers {
+		result[i] = &server.Server{
+			Host: s.Host,
+			Port: s.Port,
+		}
+	}
+	return result
 }
