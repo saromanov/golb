3d390011819e892693233ec0222fc1c2a848c5e5
Sergey <xxsmotur@gmail.com>
2019-01-19 21:27:26 +0500

    Stats: marshaling of the structure

--- golb/stats.go
+++ golb/stats.go
@@ -1,9 +1,16 @@
 package golb
 
+import "encoding/json"
+
 // Stats defines structure for the golb stats
 type Stats struct {
-	Requests         uint32 	`json:"requests"`
+	Requests         uint32         `json:"requests"`
 	StatusCodes      map[int]uint32 `json:"status_codes"`
-	CompleteRequests uint32 `json:"complete_requests"`
-	Servers          uint32 `json:"servers"`
+	CompleteRequests uint32         `json:"complete_requests"`
+	Servers          uint32         `json:"servers"`
+}
+
+// Marshal provides serialization of the Stats struct
+func (s *Stats) Marshal() ([]byte, error) {
+	return json.Marshal(s)
 }
