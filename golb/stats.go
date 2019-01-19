e04427dc03948c5a594f4d2e6d02b9415d55262b
Sergey <xxsmotur@gmail.com>
2019-01-19 21:25:42 +0500

    stats: add json attributes

--- golb/stats.go
+++ golb/stats.go
@@ -2,8 +2,8 @@ package golb
 
 // Stats defines structure for the golb stats
 type Stats struct {
-	Requests         uint32
-	StatusCodes      map[int]uint32
-	CompleteRequests uint32
-	Servers          uint32
+	Requests         uint32 	`json:"requests"`
+	StatusCodes      map[int]uint32 `json:"status_codes"`
+	CompleteRequests uint32 `json:"complete_requests"`
+	Servers          uint32 `json:"servers"`
 }
