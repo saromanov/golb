6ec44a3a847f5245e753a8b07017877f0c4146a6
Sergey <xxsmotur@gmail.com>
2019-02-15 20:46:09 +0500

    fixing of error message

--- config/consul.go
+++ config/consul.go
@@ -1,8 +1,9 @@
 package config
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
+
 	"github.com/hashicorp/consul/api"
 )
 
@@ -10,19 +11,19 @@ import (
 func ReadConsulConfig(key string) (*Config, error) {
 	client, err := api.NewClient(api.DefaultConfig())
 	if err != nil {
-		return nil, fmt.Errorf("Consul: unable to create client: %v", err)
+		return nil, fmt.Errorf("consul: unable to create client: %v", err)
 	}
 	kv := client.KV()
 
 	pair, _, err := kv.Get(key, nil)
 	if err != nil {
-		return nil, fmt.Errorf("Consul: unable to get value by the key: %v", err)
+		return nil, fmt.Errorf("consul: unable to get value by the key: %v", err)
 	}
 
-	var c*Config
+	var c *Config
 	err = json.Unmarshal(pair.Value, &c)
 	if err != nil {
-		return nil, fmt.Errorf("Consul: unable to unmarshal response: %v", err)
+		return nil, fmt.Errorf("consul: unable to unmarshal response: %v", err)
 	}
 	return c, nil
 }
